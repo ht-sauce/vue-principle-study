f21a62e35d988234ea52c4cb557e8c9ebe77a21d
dht <603928588@qq.com>
2023-09-18 14:30:56 +0800

    暂存

---
+++ vue-principle/vite.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [
+  ],
+  server: {
+    port: 8080
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
